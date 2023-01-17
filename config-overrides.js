6a09679bf59e46b57d7d629dfc4f584be75b83c0
eagleye123 <49323274+eagleye123@users.noreply.github.com>
2023-01-16 19:28:03 -0700

    addd more images and functinality

---
+++ config-overrides.js
@@ -0,0 +1,6 @@
+module.exports = function override(config, env) {
+    config.experiments = {
+      topLevelAwait: true
+    }
+    return config;
+  }
--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-import logo from './img/logo.png';
 import './App.css';
+import React, { useState } from 'react';
+// import 'dotenv/config';
+import logo from "./assests/logo.png";
+import login from "./assests/login.png"
+import githubLogo from "./assests/github.png"
+import linkedinLogo from "./assests/linkedin.png"
+import axios from 'axios'
 
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
@@ -25,12 +31,44 @@ firebase.initializeApp({
 
 })
 
+const API_KEY = 'zaBw11gYNv77O6IdJXk8V4vaVEwCGSN9sZm9i7NG';
+
+
+  async function getCalories(foodName){
+    try {
+      let response = await axios.get('https://api.nal.usda.gov/fdc/v1/foods/search?api_key='+API_KEY+'&query='+foodName);
+      let calories = response.data.foods[0].foodNutrients[3].value;
+      return {
+        cal: calories,
+        foodName: foodName
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  
+  // let {cal, foodName} = await getCalories('banana');
+
+
+const intitialFoodsBrek = [
+  { id: 1, name: 'Name', cal: 'Calories' , amount: 'amount'},
+  // { id: 2, name: foodName, cal: cal, amount: '11'}
+];
+const intitialFoodsLun = [
+  { id: 1, name: 'Name', cal: 'Calories' , amount: 'amount'},
+];
+const intitialFoodsDin = [
+  { id: 1, name: 'Name', cal: 'Calories' , amount: 'amount'},
+];
+const intitialFoodsSnack = [
+  { id: 1, name: 'Name', cal: 'Calories' , amount: 'amount'},
+];
+
+
+
 const auth = firebase.auth();
 const firestore = firebase.firestore();
-
-
-
-
 
 function SignIn(){
   const signInWithGoogle = () => {
@@ -38,7 +76,7 @@ function SignIn(){
      auth.signInWithPopup(provider);
   }
   return(
-    <button onClick={signInWithGoogle} id="user"> <img src="https://assets.stickpng.com/images/5847f9cbcef1014c0b5e48c8.png" alt="google" height="50px" /></button>
+    <button onClick={signInWithGoogle} id="user"> <img src={login} alt="google" height="50px" /></button>
   )
 }
 
@@ -50,16 +88,98 @@ function ChatRoom(){
 }
 
 function SignInFun(){
-  // const [user] = useAuthState(auth);
+  // const [user, loading, error] = useAuthState(auth);
   
   return(
     <SignIn />
-
   )
 }
 
-function App() {
+function Food({food_list}) {
+  return(
+    <div >
+    {food_list.map((food) => (
+      <div key={food.id} className='foodlayout'>
+        <p >{food.cal}</p>
+        <p>{food.name}</p>
+        <p>{food.amount}</p>
+      </div>
+    ))}
+  </div>
+  )
+}
+
+function Meal(props) {
+  return(
+    <div className='meal'>
+      <h1>{props.meal}</h1>
+  <Food food_list={props.food_list} />
+  {/* <Food /> */}
+  </div>
+)}
+
+
+
+function Menu ({foodListBrek, foodSetBrek, foodListLun, foodSetLun, foodListDin, foodSetDin, foodListSnack, foodSetSnack}){
+
+  const addBreakfast = (addedFood) => {
+    foodSetBrek([ ...foodListBrek,  addedFood]);
+  };
+  const addLunch = (addedFood) => {
+    foodSetLun([ ...foodListLun,  addedFood]);
+  };
+  const addDinner = (addedFood) => {
+    foodSetDin([ ...foodListDin,  addedFood]);
+  };
+  const addSnack = (addedFood) => {
+    foodSetSnack([ ...foodListSnack,  addedFood]);
+  };
+
+  async function handleFood(addFunction,foodToAdd) {
+    let {cal, foodName} = await getCalories(foodToAdd);
+    addFunction({ id: 2, name: foodName, cal: cal, amount: '11'})
+  }
   
+  const [inputValueBrek, setInputValueBrek] = useState("");
+  const [inputValueLun, setInputValueLun] = useState("");
+  const [inputValueDin, setInputValueDin] = useState("");
+  const [inputValueSnack, setInputValueSnack] = useState("");
+  
+
+  return(
+    <div>
+      <button className='menuItem' onClick={() => handleFood(addBreakfast, inputValueBrek)}>Breakfast</button>
+                <input type="text" value={inputValueBrek} 
+                onChange={event => setInputValueBrek(event.target.value)}/>
+
+      <button className='menuItem' onClick={() => handleFood(addLunch, inputValueLun)}>Lunch</button>
+      <input type="text" value={inputValueLun} 
+                onChange={event => setInputValueLun(event.target.value)}/>
+
+      <button className='menuItem' onClick={() => handleFood(addDinner, inputValueDin)}>Dinner</button>
+      <input type="text" value={inputValueDin} 
+                onChange={event => setInputValueDin(event.target.value)}/>
+
+      <button className='menuItem' onClick={() => handleFood(addSnack, inputValueSnack)}>Snack</button>
+      <input type="text" value={inputValueSnack} 
+                onChange={event => setInputValueSnack(event.target.value)}/>
+    </div>
+  )
+}
+
+
+
+function App() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
+  const [foodsBrek, setFoodsBrek] = useState(intitialFoodsBrek);
+  const [foodsLun, setFoodsLun] = useState(intitialFoodsLun);
+  const [foodsDin, setFoodsDin] = useState(intitialFoodsDin);
+  const [foodsSnack, setFoodsSnack] = useState(intitialFoodsSnack);
+
 
 
   return (
@@ -67,20 +187,34 @@ function App() {
     <header>
             <img height="60em" src={logo} alt="logo"/>
             <h1>LiftMate</h1>
+            
+            <h1 className='date'>Today</h1>
             <SignInFun />
     </header>
-    <main>
-      
-    
-        
+
+    <main className="main">
+      <div className="left">
+      {/* onMouseLeave={handleOpen} add this to the line below to have the menut disapear */}
+      <div className='hoverButton'  onMouseEnter={handleOpen} >Add Food</div> 
+      {open ? <Menu foodListBrek={foodsBrek} foodSetBrek={setFoodsBrek} 
+      foodListLun={foodsLun} foodSetLun={setFoodsLun}
+      foodListDin={foodsDin} foodSetDin={setFoodsDin}
+      foodListSnack={foodsSnack} foodSetSnack={setFoodsSnack}/> : <div></div>}
+      </div>
+      <div >
+        <Meal  meal="Breakfast" food_list={foodsBrek} />
+        <Meal meal="Lunch" food_list={foodsLun}/>
+        <Meal meal="Dinner" food_list={foodsDin}/>
+        <Meal meal="Snacks" food_list={foodsSnack}/>
+      </div>
     </main>
+
     <footer>
-        <nav class="footeritems">Github</nav>
-        <nav class="footeritems">Linkedin</nav>
-        <p class="footeritems">copyright</p>
+        <nav className="footeritems"> <img height='100px' src={githubLogo} alt="Github" /></nav>
+        <nav className="footeritems"><img height='37px' src={linkedinLogo} alt="LinkedIn" /></nav>
+        <p className="footeritems">Copyright 2022</p>
     </footer>
     </div>
   );
 }
-
 export default App;
